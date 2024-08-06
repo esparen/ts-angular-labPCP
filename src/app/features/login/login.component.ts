620e257cde163f2a896da3991d729969d9ffb778
Ederson Sparenberger <72260390+esparen@users.noreply.github.com>
2024-08-06 15:59:33 -0300

    Feat/forgot-credentials (#5)
    * feat(dialog): Adds dialog component for system messages and actions
    
    * feat(login): Updates login component with dialog for forgot password functionality

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -2,17 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import {
+  DialogComponent,
+  IDialogData,
+} from '../../core/components/dialog/dialog.component';
+import { Dialog, DIALOG_DATA, DialogModule } from '@angular/cdk/dialog';
+
+export interface LoginData {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, DialogModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
+
 export class LoginComponent {
   username: string = '';
   password: string = '';
+
+  constructor(private dialog: Dialog) {}
 
   login() {
     console.log('Username:', this.username);
@@ -20,7 +33,16 @@ export class LoginComponent {
   }
 
   forgotPassword() {
-    console.log('Forgot password');
+    const dialogRef = this.dialog.open<IDialogData>(DialogComponent, {
+      minWidth: '300px',
+      data: {
+        message: 'Funcionalidade não implementada',
+        actions: [{ label: 'OK', action: 'close', visible: true }],
+      },
+    });
+    dialogRef.closed.subscribe((result) => {
+      console.log('Forgot password dialog was closed');
+    });
   }
   
 }
