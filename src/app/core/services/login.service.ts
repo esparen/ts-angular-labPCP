d06c808ab3bc721b5527c836b120aa6a2207dbe6
Eder Sparenberger <edsparen@gmail.com>
2024-08-06 21:25:30 -0300

    feat(login): Adds login service, performs form validation, implements login functionality

---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, switchMap } from 'rxjs/operators';
+
+interface IDbUser {
+  id: number;
+  name: string;
+  password: string;
+  papelId: number;
+}
+
+interface IDbRole {
+  id : number;
+  name: string; 
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  role: IDbRole;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  private apiUsersUrl = 'http://localhost:3000/usuario';
+  private apiRolesUrl = 'http://localhost:3000/papel';
+
+  constructor(private http: HttpClient) {}
+
+  signIn(username: string, password: string): Observable<IUser | null> {
+    return this.findUser(username, password).pipe(
+      switchMap((user) => (user ? this.fetchUserRole(user) : of(null))),
+      catchError((error) => {
+        console.error('Error during sign-in process:', error);
+        return of(null);
+      })
+    );
+  }
+
+  private findUser(
+    username: string,
+    password: string
+  ): Observable<IDbUser | null> {
+    return this.http.get<IDbUser[]>(this.apiUsersUrl).pipe(
+      map(
+        (users) =>
+          users.find(
+            (user) => user.name === username && user.password === password
+          ) || null
+      ),
+      catchError(() => of(null))
+    );
+  }
+
+  private fetchUserRole(user: IDbUser): Observable<IUser> {
+    return this.http.get<IDbRole[]>(this.apiRolesUrl).pipe(
+      map((roles) => roles.find((role) => role.id === user.papelId)),
+      map((role) => ({
+        id: user.id,
+        name: user.name,
+        role: role || { id: 0, name: 'Unknown' },
+      }))
+    );
+  }
+}
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import {
   DialogComponent,
   IDialogData,
 } from '../../core/components/dialog/dialog.component';
-import { Dialog, DIALOG_DATA, DialogModule } from '@angular/cdk/dialog';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { LoginService } from '../../core/services/login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export interface LoginData {
   username: string;
@@ -16,15 +23,26 @@ export interface LoginData {
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, DialogModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, DialogModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
   username: string = '';
   password: string = '';
 
-  constructor(private dialog: Dialog) {}
+  constructor(
+    private fb: FormBuilder,
+    private dialog: Dialog,
+    private loginService: LoginService,
+    private snackBar: MatSnackBar
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
   login() {
     console.log('Username:', this.username);
@@ -44,7 +62,7 @@ export class LoginComponent {
     });
   }
 
-  signUp() {  
+  signUp() {
     const dialogRef = this.dialog.open<IDialogData>(DialogComponent, {
       minWidth: '300px',
       data: {
@@ -55,5 +73,19 @@ export class LoginComponent {
     dialogRef.closed.subscribe((result) => {
       console.log('Sign up dialog was closed');
     });
+  }
+
+  signIn() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.loginService.signIn(username, password).subscribe(user => {
+        if (user) 
+          this.snackBar.open(`Bem vindo, ${user.name}!`, 'Close', { duration: 5000 });
+        else
+          this.snackBar.open('Usuário não encontrado ou senha inválida', 'Close', { duration: 5000 });
+      });
+    } else {
+      this.snackBar.open('Dados incorretos, revise as mensagens de erro nos campos usuário e senha', 'Close', { duration: 5000 });
+    }
   }
 }
