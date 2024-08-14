24cd9554d9418e7a1dd9c51cde6f8108199cb34c
Eder Sparenberger <edsparen@gmail.com>
2024-08-13 21:25:26 -0300

    feat(sidebar): Adds sidebar component and update app component to include it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AppSidebarComponent } from './core/components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppSidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'labPCP - Gestão Educational';
+  constructor(private router: Router) {}
+
+  isLoginPage(): boolean {    
+    return this.router.url === '/login';
+  }
 }
