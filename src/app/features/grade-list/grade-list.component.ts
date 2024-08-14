2c0487b24308f1ca364033c76154ccc0962faeea
Eder Sparenberger <edsparen@gmail.com>
2024-08-13 23:38:09 -0300

    feat: Add grade-list and grade components

---
+++ src/app/features/grade-list/grade-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './grade-list.component.html',
+  styleUrl: './grade-list.component.scss'
+})
+export class GradeListComponent {
+
+}
