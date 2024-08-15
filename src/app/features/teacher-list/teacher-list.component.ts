8c0cd173919bfca323b6cafdbbc3bdead50ee967
Eder Sparenberger <edsparen@gmail.com>
2024-08-14 23:59:36 -0300

    feat: Update teacher-list component UI and functionality

--- src/app/features/teacher-list/teacher-list.component.ts
+++ src/app/features/teacher-list/teacher-list.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService, IUser as ITeacher } from '../../core/services/user.service';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-teacher-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './teacher-list.component.html',
-  styleUrl: './teacher-list.component.scss'
+  styleUrl: './teacher-list.component.scss',
 })
-export class TeacherListComponent {
+export class TeacherListComponent implements OnInit {
+  teachers: ITeacher[] = [];
+  filteredTeachers: ITeacher[] = [];
+  searchQuery: string = '';
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.userService.getAllTeachers().subscribe((data: ITeacher[]) => {
+      this.teachers = data;
+      this.filteredTeachers = data;
+    });
+  }
+
+  filterTeachers() {
+    if (this.searchQuery) {
+      this.filteredTeachers = this.teachers.filter(
+        (teacher) =>
+          teacher.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          teacher.id.toString().includes(this.searchQuery)
+      );
+    } else {
+      this.filteredTeachers = this.teachers;
+    }
+  }
+
+  viewTeacher(id: number) {
+    this.router.navigate(['/teacher'], {
+      queryParams: { id: id, mode: 'edit' },
+    });
+  }
 }
